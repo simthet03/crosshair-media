885622e633a3f14d919618ab10a5cedd0a20f4fb
siyabongamthethwa70 <siyabongamthethwa70@gmail.com>
2025-02-24 16:29:53 +0200

    updated tailwind module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
-      extend: {},
+    extend: {},
   },
   plugins: [],
-};
+}
